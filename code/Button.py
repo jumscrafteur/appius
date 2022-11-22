be00973df0bd66423c25b634c5c21281df8c26b2
mportefa <mael.portefaix@insa-cvl.fr>
2022-11-22 15:44:52 +0100

    class button

--- code/Button.py
+++ code/Button.py
@@ -1,24 +1,50 @@
 import pygame 
-from const import*
+from const import font1
+
 
 class Button:
     
-    def __init__(self,  x, y, height, width, image, text=""):
+    def __init__(self,  x, y, width, height, action):
         self.x= x
         self.y = y
         self.width = width
         self.height = height
-        self.image = pygame.image.load(image)
+        self.action = action
+        
+    def MouseonButton(self, posMouse):
+        if(posMouse[0] > (self.x - self.width/2) and posMouse[0] < (self.x + self.width/2)):
+            if (posMouse[1] > (self.y - self.height/2) and posMouse[1] < (self.y + self.height/2)):
+                 pygame.event.post(pygame.event.Event(
+                    pygame.USEREVENT, {"action": self.action}))
+                
+        
+    
+        
+        
+class Button_img(Button):
+    def __init__(self, x, y, action,  image):
+        self.image = pygame.image.load(image).convert()
+        super().__init__(x, y, self.image.get_width(), self.image.get_height(), action)
         self.rect = self.image.get_rect(center=(self.x, self.y))
+        
+    def show(self, screen):
+        screen.blit(pygame.transform.scale(self.image, (self.image.get_width(), self.image.get_height())), self.rect)
+    
+    
+class Button_text(Button):
+    
+    def __init__(self, x, y, action, text, font=font1):
         self.text = text
+        font = pygame.font.SysFont(font1, 60)
+        self.font = font
+        self.text_render = font.render(self.text, 1, (210,210,210), (225,225,225))
+        super().__init__(x, y, self.text_render.get_width(), self.text_render.get_height(), action)
         
-    def draw(self, screen):
-        font = pygame.font.SysFont('Bookman Old Style Std Regular', 60)
-        if self.text !="":
-            text = font.render(self.text, 1, (0,0,0))
-            screen.blit(text, (screen_height/2 - self.height/2, screen_width/2))
-        else:
-            screen.blit(pygame.transform.scale(self.image, (self.height, self.width)), self.rect)
+    def show(self, screen, border=None):
+            
+            pygame.draw.rect(screen, border, ((self.x-self.text_render.get_width()/2)-2, (self.y-self.text_render.get_height()/2)-2, self.text_render.get_width()+4, self.text_render.get_height()+4), 0)
+            screen.blit(self.text_render, (self.x-self.text_render.get_width()/2, self.y-self.text_render.get_height()/2))
         
-
+    
+    
         
