03541968fe8ad297bde106cc60fe2d5faad3c6c8
ines Oueslati <inesoueslati@MacBook-Air-de-ines.local>
2022-12-12 15:47:50 +0100

    Building

--- code/Walker.py
+++ code/Walker.py
@@ -1,30 +1,3 @@
-class Building:
-    def __init__(self):
-        self.pos = (0, 0)
-        self.type = None  # {Tent,Temples,Prefecture,Well-water..}
-        self.risk_collapse = 0  # 0:pas de risk 
-        self.capacity = 0
-        self.number_workers = 0
-        self.price_building = 0
-        self.currentNB = 0
-        self.service =False
-        self.needs =[]
-
-    def position(self):
-        return
-
-class Buildings:
-    def __init__(self):
-        ''' Une simple liste vide '''
-        self.Building = []
-
-    def ajouter(self, Building):  #ajouter 
-        self.Building.append(Building)
-
-    def retirer(self, Building):
-        self.Building.remove(Building)
-
-
 class Walker():
     def __init__(self, save):
         self.range = 0
