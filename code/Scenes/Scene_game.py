e5c17e1026193437448616cec673beccfdfe1cf3
Jumscrafteur <hsansane@gmail.com>
2022-12-31 11:18:02 +0100

    Hugo : Fin Refacto map

--- code/Scenes/Scene_game.py
+++ code/Scenes/Scene_game.py
@@ -16,47 +16,33 @@ def SceneGameCreate(self):
 
 
 def SceneGameRun(self):
-    if not REFACTO_HUGO:
-        self.map.run()
-    else:
-        self.game.screen.fill((0, 0, 0))
-        self.camera.movement_arrow()
+    self.game.screen.fill((0, 0, 0))
+    self.camera.movement_arrow()
 
-        for building in self.game.save.map:
-            # Get de position from de building
-            gridCartPosX, gridCartPosY = building.pos
+    for building in self.game.save.map:
+        # Get de position from de building
+        gridCartPosX, gridCartPosY = building.pos
 
-            # Space them according to the tile size
-            mapCartPosX, mapCartPosY = gridCartPosX * 30, gridCartPosY * 30
+        # Space them according to the tile size
+        mapCartPosX, mapCartPosY = gridCartPosX * 60, gridCartPosY * 60
 
-            # Transfer to Isometric space
-            mapIsoPosX, mapIsoPosY = cartCoToIsoCo(mapCartPosX, mapCartPosY)
+        # Transfer to Isometric space
+        mapIsoPosX, mapIsoPosY = cartCoToIsoCo(mapCartPosX, mapCartPosY)
 
-            self.game.screen.blit(building.tileImage,
-                                  (mapIsoPosX + self.camera.scroll.x, mapIsoPosY + self.camera.scroll.y))
+        self.game.screen.blit(building.tileImage,
+                              (mapIsoPosX + self.camera.scroll.x, mapIsoPosY + self.camera.scroll.y))
 
-        self.map.hudleft.draw(self.game.screen)
-        # self.map.hudstick.draw(self.game.screen)
-        self.map.hudup.draw(self.game.screen)
-        self.map.infofps.draw(self.game.screen)
-        self.map.infopop.draw(self.game.screen)
+    self.map.hudleft.draw(self.game.screen)
+    self.map.hudup.draw(self.game.screen)
+    self.map.infofps.draw(self.game.screen)
+    self.map.infopop.draw(self.game.screen)
 
-        pygame.display.flip()
+    pygame.display.flip()
 
 
 def SceneGameHandleEvents(self, event):
-    if REFACTO_HUGO:
-        if event.type in [pygame.KEYUP, pygame.KEYDOWN] and event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_DOWN, pygame.K_UP]:
-            self.camera.keys[event.key] = not self.camera.keys[event.key]
-
-        # self.camera.movement_arrow(pygame.key.get_pressed())
-
-
-# 1073741906
-# 1073741903
-
-# 1073741905
-# 1073741904
+    if event.type in [pygame.KEYUP, pygame.KEYDOWN] and event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_DOWN, pygame.K_UP]:
+        self.camera.keys[event.key] = not self.camera.keys[event.key]
 
 
 SCENE = Scene(SCENE_GAME_ID, 'Scene_Menu', createFunc=SceneGameCreate,
