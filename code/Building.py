cfb9a4e30b629a709ded1cf001c4ebb33c0c9d0f
ines Oueslati <inesoueslati@MacBook-Air-de-ines.local>
2022-12-31 14:50:56 +0100

    changement au niveau classe Building et walker

--- code/Building.py
+++ code/Building.py
@@ -1,41 +1,210 @@
 import pygame
-
+import random
 
 class Building:
     def __init__(self, pos):
         self.pos = pos
-        self.type = None  # {Tent,Temples,Prefecture,Well-water..}
         self.risk_collapse = 0  # 0:pas de risk
-        self.capacity = 0
         self.number_workers = 0
         self.price_building = 0
-        self.currentNB = 0
         self.service = False
         self.needs = []
-
         self.tileImage = None
+        
 
-    # def position(self):
-    #     return
+    def _get_pos(self):
+        return self.pos
 
+    def _get_riskfeu(self):
+        return self.riskfeu
+
+    def _get_riskcollapse(self):
+        return self.riskcollapse
+
+    def update_riskfeu(self,Prefect):           
+        assert(type(Prefect)==Prefect) 
+        return 
+    
+    def update_riskcollapse(self,Engineer):      
+        assert(type(Engineer)==Engineer)
+        return
+
+class Tent (Building) :
+    def __init__(self,pos):
+        super.__init__(self,pos)
+        self.type='Tent'
+        self.capacity=0 #par défaut 
+        self.currentNB=0
+        self.statut= {"Panneau":1,"Construction":0,"Tent":0,"T_feu":0,"T_collapse":0} # statut du batiment
+    
+    def up_date_statut(self,): # a faire
+        return None
+        
+    
+class prefecture(Building): 
+    def __init__(self,pos):
+        super.__init__(self,pos)
+        self.type='Prefecture'
+        self.statut= {"Prefecture":1,"P_feu":0,"P_collapse":0}
+    
+    def up_date_statut():
+        return None        
+
+class water_well(Building): ##puit
+    def __inti__(self,pos):
+        super.__init__(self,pos)
+        self.type='water_well'
+
+
+class Senat(Building):
+    def __init__(self,pos):
+        super.__init__(self,pos)
+        self.type='Senat'
+        self.statut= {"Senat":1,"S_feu":0,"S_collapse":0}
+        self.umployment=0
+        self.culture_level=0
+        self.Peace_level=0
+        self.attractivity=0
+        self.population=0
+    
+    def calcul_culture_level(self):
+        return 
+    def calcul_Peace_level(self):
+        return 
+    def calcul_attractuvity(self):
+        return
+    def calcul_populaton(self):
+        return
+    def up_date_statut():
+        return None
+
+
+class B_engineering(Building):
+    def __init__(self,pos):
+        super.__init__(self,pos)
+        self.type='B_Engineering'
+        self.statut= {"B":1,"B_feu":0,"B_collapse":0}
+
+
+
+''''sousclasse pour les temples'''
+class Temples(Building):
+    def __init__(self,pos):
+        super.__init__(self,pos)
+        self.type='Temples'
+        self.nb_temples=0
+
+    def rate_satisfaction():        # a faire seulement le nombre des temples et le taille de la mapp
+
+        return 
+
+class Ceres(Temples):
+    def __init__(self,pos):
+        super.__init__(self,pos)
+        self.religion='Ceres'
+    
+    def rate_satisfaction():        
+        return 
+        
+class Mars(Temples):
+    def __init__(self,pos):
+        super.__init__(self,pos)
+        self.religion='Mars'
+    
+    def rate_satisfaction():        
+        return 
+        
+class Mercury(Temples):
+    def __init__(self,pos):
+        super.__init__(self,pos)
+        self.religion='Mercury'
+    
+    def rate_satisfaction():        
+        return 
+        
+class Neptune (Temples):
+    def __init__(self,pos):
+        super.__init__(self,pos)
+        self.religion='Neptune'
+
+    def rate_satisfaction():        
+        return 
+        
+class Venus(Temples):
+    def __init__(self,pos):
+        super.__init(self,pos)
+        self.religon='Venus'
+    
+    def rate_satisfaction():        
+        return 
+    
+class Buildings:
+    def __init__(self):
+        ''' Un dict de liste vide '''
+        self.listBuilding = {"Tent":[],"Prefecture":[],"B_Engineering":[],"water_well":[],"Senat":[],"Temples":{"Ceres":[],"Mars":[],"Mercury":[],"Neptune":[],"Venus":[]}}
+
+    def __iter__(self):                        #
+        return iter(self.Building)
+
+    def add_Tent(self, T): 
+        assert(type(T)==Tent)
+        self.Building["Tent"].append(T)
+    def add_prefecture(self,P):
+        assert(type(P)==prefecture)
+        self.Building["Prefecture"].append(P)
+    def add_B_Engineering(self,B):
+        assert(type(B)==B_engineering)
+        self.Building["B_Enginneering"].append(B)
+    def add_water_well(self,W):
+        assert(type(W)==water_well)
+        self.Building["water_well"].append(W)
+    def add_Senat(self,S):
+        assert(type(S)==Senat)
+        self.Building["Senat"].append(S)
+    def add_Temples(self,Temple):
+        if(type(Temple)==Ceres):
+            self.Building["Temples"]["Ceres"].append(Temple)
+        elif(type(Temple)==Mars):
+            self.Building["Temples"]["Mars"].append(Temple)
+        elif(type(Temple)==Mercury):
+            self.Building["Temples"]["Mercury"].append(Temple)
+        elif(type(Temple)==Neptune):
+            self.Building["Temples"]["Neptune"].append(Temple)
+        elif(type(Temple)==Venus):
+            self.Building["Temples"]["Venus"].append(Temple)
+        else:
+            exit
+
+    def remove_tent(self,T):
+        assert(type(T)==Tent)
+        self.Building["Tent"].remove(T)
+    def remove_prefecture(self,P):
+        assert(type(P)==prefecture)
+        self.Building["Prefecture"].remove(P)
+    def remove_B_Engineering(self,B):
+        assert(type(B)==B_engineering)
+        self.Building["B_Enginneering"].remove(B)
+    def remove_water_well(self,W):
+        assert(type(W)==water_well)
+        self.Building["water_well"].remove(W)
+    def remove_senat(self,S):
+        assert(type(S)==Senat)
+        self.Building["Senat"].remove(S)
+    def remove_temples(self,Temple):
+        if(type(Temple)==Ceres):
+            self.Building["Temples"]["Ceres"].remove(Temple)
+        elif(type(Temple)==Mars):
+            self.Building["Temples"]["Mars"].remove(Temple)
+        elif(type(Temple)==Mercury):
+            self.Building["Temples"]["Mercury"].remove(Temple)
+        elif(type(Temple)==Neptune):
+            self.Building["Temples"]["Neptune"].remove(Temple)
+        elif(type(Temple)==Venus):
+            self.Building["Temples"]["Venus"].remove(Temple)
+        else:
+            exit
 
 class Grass(Building):
     def __init__(self, pos):
         super().__init__(pos)
         self.tileImage = pygame.image.load("newland/Land1a_00285.png")
-        # self.tileImage = pygame.image.load("newland/Land1a_00035.png")
-
-
-class Buildings:
-    def __init__(self):
-        ''' Une simple liste vide '''
-        self.Building = []
-
-    def __iter__(self):
-        return iter(self.Building)
-
-    def ajouter(self, Building):  # ajouter
-        self.Building.append(Building)
-
-    def retirer(self, Building):
-        self.Building.remove(Building)
--- code/Walker.py
+++ code/Walker.py
@@ -1,3 +1,4 @@
+import Building
 class Walker():
     def __init__(self, save):
         self.range = 0
@@ -24,15 +25,17 @@ class Engineer(Walker):
     def __init__(self, save):
         Walker.__init__(self, save)
         self.type = "Engineer"
+        self.unemployed=False
 
     def work(self, Buildings):
-        assert(type(Buildings)==Buildings) #ca pour forcer que le builfdings est de type Buildings qui est la listee des batiments 
+        assert(type(Buildings)==Buildings)
         r = self.rayonDAction
         for i in range(self.pos[0]-r, self.pos[0]+r):
             for j in range(self.pos[1]-r, self.pos[1]+r):
-                for b in Buildings.building:
-                    if ((i, j) == b.pos) and (b.risk_collapse > 0):
-                        b.risk_collapse = 0
+                for b in Buildings.listBuilding.keys():                 # a refaire 
+                    for k in Buildings.listBuilding[b]:
+                        if ((i, j) == k.pos) and (k.risk_collapse > 0):
+                            k.risk_collapse = 0
       
 
 
@@ -40,11 +43,12 @@ class Prefect(Walker):
     def __init__(self,save):
         Walker.__init__(self,save)
         self.type = "Prefect"
+        self.unemployed=False
 
     def work(self,save):
         MR = save.layers["risk_feu"] 
         r = self.rayonDAction
-        for i in range(self.pos[0]-r, self.pos[0]+r):
+        for i in range(self.pos[0]-r, self.pos[0]+r):  # a refaire 
             for j in range(self.pos[1]-r, self.pos[1]+r):
                 MR[i][j] = 0
 
@@ -56,10 +60,63 @@ class Citizen(Walker):
 
     def work(self, save, Buildings):
         r = self.rayonDAction
-        assert(type(Buildings)==Buildings) # de meme ici 
+        assert(type(Buildings)==Buildings)
         for i in range(self.pos[0]-r, self.pos[0]+r):
             for j in range(self.pos[1]-r, self.pos[1]+r):
-                for b in Buildings.building:
+                for b in Buildings.listBuilding["Tent"]:
                     if ((i, j) == b.pos) and (b.currentNB < b.capacity):
                         b.currentNb += 1
                         break   
+        
+
+class Walkers():
+        #une sorte de base de donnée pour les Walkers 
+        #sauvegarde des Walkers dans un dict selon leurs types 
+        #sauvegarde des citizen dans une liste
+    def __init__(self):
+        self.listWalker = {"Citizen":[],"Prefect":[],"Engineer":[]}
+        self.pop=0
+  
+    def _get_pop(self):
+        return self.pop
+    
+    def _set_pop(self,p):
+        self.pop=p
+
+    def calcul_pop(self): 
+            self.pop=len(self.listWalker["Citizen"])+len(self.listWalker["Prefect"])+len(self.listWalker["Engineer"])
+            
+    def unemployment_rate(self):
+        l=self.listWalker["Citizen"]
+        self.calcul_pop
+        assert self.pop>0,"la population est nulle"
+        nb=0
+        for i in l:
+           if(i.unemployed):
+            nb+=1
+        return nb/self._get_pop(self)
+  
+    #Gestion des citizens 
+    def ajout_Citizen(self,C):
+        assert(type(C)==Citizen)
+        self.ListWalker["Citizen"].append(C)
+     
+    def supp_Citizen(self,C):
+        assert(type(C)==Citizen)
+        self.ListWalker["Citizen"].remove(C)
+  
+    #Gestion de Prefet
+    def ajout_Prefet(self,P):
+        assert(type(P)==Prefect)
+        self.ListWalker["Prefect"].append(P)
+    def supp_Prefet(self,P):
+        assert(type(P)==Prefect)
+        self.ListWalker["Prefect"].remove(P)
+
+    #Gestion de Engineer
+    def ajout_Engineer(self,E):
+        assert(type(E)==Engineer)
+        self.ListWalker["Engineer"].append(E)
+    def supp_Engineer(self,E):
+        assert(type(E)==Engineer)
+        self.ListWalker["Engineer"].remove(E)
