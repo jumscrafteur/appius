eed7225121317052e5f4bc1441fcad5fbfd9d333
Yay589 <114221434+Yay589@users.noreply.github.com>
2023-01-03 15:41:30 +0100

    Add files via upload

--- code/gameUpdateActivities.py
+++ code/gameUpdateActivities.py
@@ -5,14 +5,22 @@
 #prospérité en %
 
 import Buildings
+    
 
 class gameUpdate:
-    building=list(Buildings.building)
     def attractivity(fireRisk,collapseRisks,criminalityRisks):
         return (1.5-((fireRisk+collapseRisks+criminalityRisks)/1.5))*100
     
-    def prosperity(building):
+    def prosperity(buildings):
         sumActractivities=0
-        for i in building:
+        for i in buildings:
             sumActractivities+=gameUpdate.attractivity(i.risk_fire,i.risk_collapse,i.risk_criminality)
-        return sumActractivities/len(building)
+        return sumActractivities/len(buildings)
+while game_Running:
+    time.sleep(30)
+    for i in list(Buildings.building):
+        i.fireRisk=
+        i.collapseRisks=
+        i.criminalityRisks=
+        Attractivity=gameUpdate.attractivity(i.fireRisk, i.collapseRisks, i.criminalityRisks)     
+    Prosperity=gameUpdate.prosperity(Buildings.building)
