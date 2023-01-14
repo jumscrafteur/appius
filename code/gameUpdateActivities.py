aa871e0bd522b5da3117b67ca13d2682f152b277
Yay589 <114221434+Yay589@users.noreply.github.com>
2023-01-14 12:19:07 +0100

    Add files via upload

--- code/gameUpdateActivities.py
+++ code/gameUpdateActivities.py
@@ -14,9 +14,9 @@ class gameUpdate:
     if CompteurFps%60==0:
         #update les atributs des batiments
         for i in Buildings.building:
-            i.fireRisk=fire_Risks(i.pos,Prefect().pos)
-            i.collapseRisks=collapse_Risks(i.pos,getMinDistance(Engineer().pos)
-            i.criminalityRisks=criminality_Risks(i.pos,getMinDistanceBetwiHomes(i.pos,"Prefecture"))
+            i.fireRisk=fire_Risks(i.pos,getMinDistanceBetweenWolkersHome(i.pos,"Prefect"))
+            i.collapseRisks=collapse_Risks(i.pos,getMinDistanceBetweenWolkersHome(i.pos,"Engineer"))
+            i.criminalityRisks=criminality_Risks(i.pos,getMinDistanceBetweenHomes(i.pos,"Prefecture"))
             Attractivity=gameUpdate.attractivity(i.fireRisk, i.collapseRisks, i.criminalityRisks)     
         Prosperity=gameUpdate.prosperity(Buildings.building)
         
@@ -29,25 +29,28 @@ class gameUpdate:
             sumActractivities+=gameUpdate.attractivity(i.risk_fire,i.risk_collapse,i.risk_criminality)
         return sumActractivities/len(buildings)
 
-    def getMinDistanceWolkerAndHome(homePosition,typeWalker):#renvoie la position du walker le plus proche d'une tente
+    def getMinDistanceBetweenWolkersHome(homePosition,typeWalker):#renvoie la position du walker le plus proche d'une tente
         listWolkers=Walkers.ListWalker[typeWalker]
         indexMin=0
         for i in range(len(listWolkers)-1): 
-            if Distance(homePosition,listWolkers[i+1].pos)<Distance(homePosition,listWolkers[i].pos):
+            if Manhanttan_Distance(homePosition,listWolkers[i+1].pos)<Distance(homePosition,listWolkers[i].pos):
                 indexMin=i+1
         return listWolkers[idexMin].pos
             
-    def getMinDistanceBetwiHomes(homePosition,TypeLocation):#renvoi le batiment le plus proche d'une position lié à une tente
+    def getMinDistanceBetweenHomes(homePosition,TypeLocation):#renvoi le batiment le plus proche d'une tente
         listTypeLocation=[]
         for i in Buildings.building:
-           if i.type==TypeLocation:
+            if i.type==TypeLocation:
                listTypeLocation.append(i)
         indexMin=0
         for i in range(len(listTypeLocation)-1): 
             if Distance(homePosition,listTypeLocation[i+1].pos)<Distance(homePosition,listTypeLocation[i].pos):
                 indexMin=i+1
-        return listTypeLocation[idexMin].pos
+       ### exp: p1 est de la forme p1={x,y]
+       def Distance(p1,p2):
+           return abs(p2[1]-p1[1])+abs(p2[2]-p1[2])
+        
+       return listTypeLocation[idexMin].pos
     
     
-S
             
