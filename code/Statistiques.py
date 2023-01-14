200a618ad95c10b34474361d914e3cc59d4d16b2
Yay589 <114221434+Yay589@users.noreply.github.com>
2023-01-14 12:21:23 +0100

    Add files via upload

--- code/Statistiques.py
+++ code/Statistiques.py
@@ -7,39 +7,20 @@ la clase Buldings et workers
 class Stat:
     def fire_Risks(homeLocation,prefetCurrentLocation):
         Distance_=distance(homeLocation,prefetCurrentLocation)
-        if (Distance_>10):
-            return 0.5
-        elif (Distance_>10):
-            return 0.4
-        elif (Distance_>10):
-            return 0.2
-        elif (Distance_>10):
+        if (Distance_>1):
             return 0
-        return False
+       
     def criminality_Risks(homeLocation,prefetOfficeLocation):
         Distance_=distance(homeLocation,prefetOfficeLocation)
-        if (Distance_>10):
-            return 0.5
-        elif (Distance_>10):
-            return 0.4
-        elif (Distance_>10):
-            return 0.2
-        elif (Distance_>10):
+        if (Distance_>1:
             return 0
-        return False 
+         
     def collapse_Risks(homeLocation,ingineerLocation):
         Distance_=distance(homeLocation,ingineerLocation)
-        if (Distance_>10):
-            return 0.5
-        elif (Distance_>10):
-            return 0.4
-        elif (Distance_>10):
-            return 0.2
-        elif (Distance_>10):
+        if (Distance_>1):
             return 0
-        return Falses
     def attractivity(fire,collapse,criminality):
-        return (1.5-((fire+collapse+criminality)/1.5))*100
+        return (300-((fire+collapse+criminality)/300))*100
     
     
     
